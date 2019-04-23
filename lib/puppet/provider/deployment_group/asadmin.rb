0a3016f5001e38679368e3686e05e63b1f5c073f
Andy Brown <andy@techdad.tech>
2019-04-23 19:51:49 +0100

    got cluster / deployment group mixed up

--- lib/puppet/provider/deployment_group/asadmin.rb
+++ lib/puppet/provider/deployment_group/asadmin.rb
@@ -1,14 +1,14 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__),"..","..",".."))
 require 'puppet/provider/asadmin'
 
-Puppet::Type.type(:cluster).provide(:asadmin,
+Puppet::Type.type(:deployment_group).provide(:asadmin,
                                    :parent => Puppet::Provider::Asadmin) do
   desc "Glassfish Cluster support."
 
   def create
     # Start a new args array
     args = Array.new
-    args << "create-cluster"
+    args << "create-deployment-group"
     args << "--gmsenabled" << @resource[:gmsenabled] if @resource[:gmsenabled]
     args << "--multicastport" << @resource[:multicastport] if @resource[:multicastport]
     args << "--multicastaddress" << @resource[:multicastaddress] if @resource[:multicastaddress]
@@ -21,14 +21,14 @@ Puppet::Type.type(:cluster).provide(:asadmin,
 
   def destroy
     args = Array.new
-    args << "delete-cluster" << @resource[:name]
+    args << "delete-deployment-group" << @resource[:name]
     asadmin_exec(args)
   end
 
   def exists?
-    asadmin_exec(["list-clusters"]).each do |line|
-      cluster = line.split(" ")[0] if line.match(/running/) # Glassfish > 3.0.1
-      return true if @resource[:name] == cluster
+    asadmin_exec(["list-deployment-groups"]).each do |line|
+      deployment_group = line.split(" ")[0] if line.match(/running/) # Glassfish > 3.0.1
+      return true if @resource[:name] == deployment_group
     end
     return false
   end
